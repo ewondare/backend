6791f693a35c7aaef55467762d4f2c64a3af5104
parinazkanan <parinaz.kanan@gmail.com>
2024-01-02 09:08:54 +0330

    Implement Login, Logout, register-applicant, register-redruiter, and connected to database

---
+++ resume/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+
+class Resume(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    name = models.CharField(max_length=100 , null=True , blank=True)
+    lastName = models.CharField(max_length=100, null=True, blank=True)
+    age = models.IntegerField( null=True, blank=True)
+    gender = models.IntegerField(null=True, blank=True)
+    about = models.CharField(max_length=1000 , null=True, blank=True)
+    phone_number = models.BigIntegerField(null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=1000, null=True, blank=True)
+    # insert cv 
+    def __str__(self):
+        return f'{self.name} {self.lastName}'
---
+++ users/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login , logout
+from .models import User
+from .form import RegisterUserForm
+from resume.models import Resume
+from company.models import Company
+# Create your views here.
+
+# register applicant 
+def register_applicant(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.is_applicant = True
+            var.username = var.email
+            var.save()
+            Resume.objects.create(user=var)
+            messages.info(request , 'Your account has been created. Please log in.')
+            return redirect('login')
+        else:
+            error_message = form.errors.as_text()
+            messages.warning(request, f'Something went wrong: {error_message}')
+            return redirect('register-applicant')
+            #messages.warning(request, 'Something went wrong.')
+            #return redirect('register-applicant')
+    else:
+        form = RegisterUserForm()
+        context = {'form': form}
+        return render(request,'users/register-applicant.html' , context)
+    
+# register recruiter only
+def register_recruiter(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.is_recruiter = True
+            var.username = var.email
+            var.save()
+            Company.objects.create(user=var)
+            messages.info(request , 'Your account has been created. Please log in.')
+            return redirect('login')
+        else:
+            messages.warning(request, 'Something went wrong.')
+            return redirect('register-recruiter')
+    else:
+        form = RegisterUserForm()
+        context = {'form': form}
+        return render(request,'users/register-recruiter.html' , context)
+
+
+# login a user
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=email , password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            if request.user.is_applicant:
+                return redirect('applicant-dashboard')
+            elif request.user.is_recruiter:
+                return redirect('recruiter-dashboard')
+            else: 
+                return redirect('login')
+        else:
+            messages.warning(request, 'Something went wrong.')
+            return redirect('login')
+    else:
+        return render(request , 'users/login.html')
+    
+
+# logout user
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'Your session has ended.')
+    return redirect('login')
