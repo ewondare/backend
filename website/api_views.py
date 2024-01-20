2bbcfc034f2b6c967a9b17b600fe3092450b450e
mohammadsaeedarvenaghi <mohammadsaeedarvenaghi@gmail.com>
2024-01-20 11:32:48 +0330

    Add website APIs

--- website/api_views.py
+++ website/api_views.py
@@ -3,17 +3,25 @@ from rest_framework.response import Response
 from .serializers import JobSerializer
 from job.models import Job
 
+
 @api_view(['GET'])
-def job_listing_api(request):
-    jobs = Job.objects.filter(is_available=True)
+def last_jobs_api(request):
+    jobs = Job.objects.filter(is_available=True).order_by('-timestamp')
     serializer = JobSerializer(jobs, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
-def job_details_api(request, pk):
-    try:
-        job = Job.objects.get(pk=pk)
-        serializer = JobSerializer(job)
-        return Response(serializer.data)
-    except Job.DoesNotExist:
-        return Response({'message': 'Job not found'}, status=404)
+def search_job_api(request):
+    title = request.GET.get('title')
+    location = request.GET.get('location')
+    
+    jobs = Job.objects.filter(is_available=True)
+
+    if title:
+        jobs = jobs.filter(title__icontains=title)
+    
+    if location:
+        jobs = jobs.filter(company__location__icontains=location)
+    
+    serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data)
