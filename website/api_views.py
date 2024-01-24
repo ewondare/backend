6395e16c58d1b0e0626dd72f6cd33df4914a0102
mohammadsaeedarvenaghi <mohammadsaeedarvenaghi@gmail.com>
2024-01-24 14:41:53 +0330

    Add exceptions to users models

--- website/api_views.py
+++ website/api_views.py
@@ -6,22 +6,32 @@ from job.models import Job
 
 @api_view(['GET'])
 def last_jobs_api(request):
-    jobs = Job.objects.filter(is_available=True).order_by('-timestamp')
-    serializer = JobSerializer(jobs, many=True)
-    return Response(serializer.data)
+    try:
+        jobs = Job.objects.filter(is_available=True).order_by('-timestamp')
+        serializer = JobSerializer(jobs, many=True)
+        return Response(serializer.data)
+    except Exception as e:
+        error_message = 'An error occurred while retrieving the last jobs.'
+        response_data = {'message': error_message, 'error': str(e)}
+        return Response(response_data, status=400)
 
 @api_view(['GET'])
 def search_job_api(request):
-    title = request.GET.get('title')
-    location = request.GET.get('location')
-    
-    jobs = Job.objects.filter(is_available=True)
+    try:
+        title = request.GET.get('title')
+        location = request.GET.get('location')
 
-    if title:
-        jobs = jobs.filter(title__icontains=title)
-    
-    if location:
-        jobs = jobs.filter(company__location__icontains=location)
-    
-    serializer = JobSerializer(jobs, many=True)
-    return Response(serializer.data)
+        jobs = Job.objects.filter(is_available=True)
+
+        if title:
+            jobs = jobs.filter(title__icontains=title)
+
+        if location:
+            jobs = jobs.filter(company__location__icontains=location)
+
+        serializer = JobSerializer(jobs, many=True)
+        return Response(serializer.data)
+    except Exception as e:
+        error_message = 'An error occurred while searching for jobs.'
+        response_data = {'message': error_message, 'error': str(e)}
+        return Response(response_data, status=400)
